a67971b0c993d74eaf4dc296f65a27bb80bfbae0
adyapragnya <adyapragnya@gmail.com>
2025-02-24 19:14:07 +0530

    first commit

---
+++ backend/models/PolygonGeofence.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const PolygonGeofenceSchema = new mongoose.Schema({
+    geofenceId: String,
+    geofenceName: String,
+    type: { type: String, default: "Polygon" }, 
+    geofenceType: String,
+    seaport: String,
+    date: String,
+    remarks: String,
+    seaport: String,
+    coordinates: Array,
+    
+  }, { timestamps: true });
+
+const PolygonGeofence = mongoose.model('polygongeofences', PolygonGeofenceSchema , 'polygongeofences');
+
+export default  PolygonGeofence ;
